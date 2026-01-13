7ccdbd6c271a1eb06df19421baa63734e5e1ceea
Fabio <foli.brondix@gmail.com>
2026-01-13 21:13:47 +0100

    commit di prova nuova repo

--- backend/src/main/java/com/exercise/progetto_individuale/controllers/GroupController.java
+++ backend/src/main/java/com/exercise/progetto_individuale/controllers/GroupController.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;

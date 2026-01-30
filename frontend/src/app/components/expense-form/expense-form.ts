7a3b3280a49790c72e38eb17501a537cf296185c
Fabio <foli.brondix@gmail.com>
2026-01-30 21:02:52 +0100

    Improved UX on ExpenseForm component

--- frontend/src/app/components/expense-form/expense-form.ts
+++ frontend/src/app/components/expense-form/expense-form.ts
@@ -7,6 +7,8 @@ import { ActivatedRoute } from '@angular/router';
 
 type ExpenseRow = {
   participantId: string | null;
+  tempShare: number | null;
+  tempPaid: number | null;
 };
 @Component({
   selector: 'app-expense-form',
@@ -16,7 +18,7 @@ type ExpenseRow = {
 })
 export class ExpenseForm {
   expensePost:ExpensePost = {title:"", date:"", expenseParticipants:{}};
-  expenseRows: ExpenseRow[] = [{ participantId: null }];
+  expenseRows: ExpenseRow[] = [{ participantId: null, tempShare:null, tempPaid:null }];
 
   constructor(public eServ:ExpenseService, public gServ:GroupService, private route:ActivatedRoute)
   {
@@ -25,10 +27,10 @@ export class ExpenseForm {
 
   getGroupInfo()
   {
-    const id = this.route.snapshot.paramMap.get("id");
-    const partId = this.route.snapshot.paramMap.get("partId");
     if(!this.gServ.openedGroup)
     {
+      const id = this.route.snapshot.paramMap.get("id");
+      const partId = this.route.snapshot.paramMap.get("partId");
       this.gServ.getGroupDetail(id, partId).subscribe(
           (resp) =>
           {
@@ -53,7 +55,7 @@ export class ExpenseForm {
   addExpenseParticipant()
   {
     if(this.maxParticipantCheck())
-      this.expenseRows.push({ participantId: null });
+      this.expenseRows.push({ participantId: null, tempShare:null, tempPaid:null  });
   }
 
   maxParticipantCheck()
@@ -61,23 +63,49 @@ export class ExpenseForm {
     return this.expenseRows.length < this.gServ.openedGroup!.participants.length;
   }
 
+  onParticipantChange(row:ExpenseRow)
+  {
+    if(!row.participantId)
+      return;
+    const participant = this.expensePost.expenseParticipants[row.participantId];
+    if (row.tempShare !== null) {
+      participant.share = row.tempShare;
+    }
+
+    if (row.tempPaid !== null) {
+      participant.paidAmount = row.tempPaid;
+    }
+  }
+
   getShare(row: any): number | null {
-    if (!row.participantId) return null;
-    return this.expensePost.expenseParticipants[row.participantId].share;
+    // if (!row.participantId) return row.tempShare;
+    // return this.expensePost.expenseParticipants[row.participantId].share;
+    return row.tempShare;
   }
 
   setShare(row: any, value: number) {
-    if (!row.participantId) return;
+    if (!row.participantId)
+    {
+      row.tempShare = value;
+      return;
+    }
+    row.tempShare = value;
     this.expensePost.expenseParticipants[row.participantId].share = value;
   }
 
   getPaid(row: any): number | null {
-    if (!row.participantId) return null;
-    return this.expensePost.expenseParticipants[row.participantId].paidAmount;
+    // if (!row.participantId) return row.tempPaid;
+    // return this.expensePost.expenseParticipants[row.participantId].paidAmount;
+    return row.tempPaid;
   }
 
   setPaid(row: any, value: number) {
-    if (!row.participantId) return;
+    if (!row.participantId)
+    {
+      row.tempPaid = value;
+      return;
+    }
+    row.tempPaid = value;
     this.expensePost.expenseParticipants[row.participantId].paidAmount = value;
   }
  }
